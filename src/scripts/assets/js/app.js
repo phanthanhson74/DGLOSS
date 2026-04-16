22ab79deba311a59394097d6d57a9ec75f92022c
LeThiMyKieu <kieuqt98@gmail.com>
2026-04-16 21:52:14 +0700

    k1604

--- src/scripts/assets/js/app.js
+++ src/scripts/assets/js/app.js
@@ -3,141 +3,123 @@
 //*********************************
 //*** common
 //*********************************
-document.addEventListener("DOMContentLoaded", function () {
-  const firstText = document.querySelector(".text.first");
-  const secondText = document.querySelector(".text.second");
-
-  setTimeout(() => {
-    firstText.classList.add("visible");
-  }, 100);
-
-  setTimeout(() => {
-    secondText.classList.add("visible");
-  }, 500);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const floatBtns = document.querySelector(".float-btns");
-  const footer = document.querySelector("footer");
-
-  window.addEventListener("scroll", () => {
-      if (!floatBtns || !footer) return;
-
-      const footerRect = footer.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-
-      if (footerRect.top < windowHeight) {
-          floatBtns.classList.add("hidden");
-      } else {
-          floatBtns.classList.remove("hidden");
-      }
-  });
-});
-
-
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  paginationClickable: true,
-  spaceBetween: 20,
-  slidesPerView: 1,
-  autoHeight: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: "auto",
-    },
-  },
-});
-
-const swiper02 = new Swiper(".swiper02", {
-  loop: true,
-  paginationClickable: true,
-  spaceBetween: 20,
-  slidesPerView: 1,
-  autoHeight: true,
-  navigation: {
-    nextEl: ".swiper-button-next02",
-    prevEl: ".swiper-button-prev02",
-  },
-  pagination: {
-    el: ".swiper-pagination02",
-  },
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: "auto",
-    },
-  },
-});
 
 $(function () {
   var smoothScroll = function () {
     var $header = $(".page-header");
     var headerHeight = $header.innerHeight();
     var speed = 500;
+
     var smoothScrollC = {
       init: function () {
         var me = this;
+
         $('a[href^="#"]').on("click", function (e) {
           e.preventDefault();
           me.targetScroll($(this));
         });
       },
+
       targetScroll: function ($target) {
         var $hash = $($target.attr("href"));
+
         if ($hash.length) {
           $("html, body").animate(
             {
               scrollTop: $hash.offset().top,
             },
             speed,
-            "swing"
+            "swing",
           );
         }
       },
     };
+
     var smoothScrollParam = {
       location: location.pathname,
+
       init: function () {
         var me = this;
+
         if (!this.location.match("/admin/")) {
           if (location.search.match("anc=")) {
             me.anchor = location.search.split("anc=")[1];
           } else {
             me.anchor = location.search.split(/\?/)[1];
           }
-          var hashP = "#" + this.anchor;
-          $("html, body").animate(
-            {
-              scrollTop: $(hashP).offset().top,
-            },
-            speed,
-            "swing"
-          );
+
+          var hashP = "#" + me.anchor;
+          var $target = $(hashP);
+
+          if ($target.length) {
+            $("html, body").animate(
+              {
+                scrollTop: $target.offset().top,
+              },
+              speed,
+              "swing",
+            );
+          }
         }
       },
     };
+
     $(function () {
       smoothScrollC.init();
+
       if (location.href.match(/\?/)) {
         setTimeout(function () {
           smoothScrollParam.init();
-        }, 100);
+        }, 300);
       }
     });
   };
+
   smoothScroll();
 });
+
+/* Header Function
+ ********************************************** */
+const initHeader = () => {
+  const header = document.querySelector(".l-header");
+  const headerHamburger = document.querySelector(".js-hamburger");
+  const navToggles = document.querySelectorAll(".js-toggle-nav");
+  const headerNav = document.querySelector(".l-header__nav");
+
+  const handleHeaderState = () => {
+    const isExpanded = headerHamburger.getAttribute("aria-expanded") === "true";
+
+    const shouldActive = isExpanded || window.scrollY > 10;
+
+    header.classList.toggle("active", shouldActive);
+  };
+
+  navToggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      const isExpanded =
+        headerHamburger.getAttribute("aria-expanded") === "true";
+
+      headerHamburger.setAttribute("aria-expanded", String(!isExpanded));
+      headerNav.classList.toggle("is-active");
+      document.documentElement.classList.toggle("no-scroll");
+
+      handleHeaderState();
+    });
+  });
+
+  let ticking = false;
+
+  window.addEventListener("scroll", () => {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        handleHeaderState();
+        ticking = false;
+      });
+      ticking = true;
+    }
+  });
+
+  handleHeaderState();
+};
+
+initHeader();
